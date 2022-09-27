ead836a83a7655f7af2c04674a3c81e476690f06
arnau vincent <rnv4252a@ens.fsi>
2022-09-27 19:55:50 +0200

    cassoulet

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -9,6 +9,8 @@ public class Druide {
 		this.nom = nom;
 		this.effetPotionsMin = effetPotionsMin;
 		this.effetPotionsMax = effetPotionsMax;
+		parler("Bonjour, je suis le druide ", + nom + " et ma potion leut aller d'une force " + effetPotionsMin + " à " + effetPotionsMax + "." );
+		
 	}
 
 	public String getNom() {
